7b4344e952c67abf1394cac89bec54b45e79ab86
sys465 <sys465@gmail.com>
2021-12-01 21:37:24 +0300

    еще один проект, обертки, выдает две критические ошибки но собирается))

---
+++ _wrappers/src/com/javastart/_wrappers/Main.java
@@ -0,0 +1,14 @@
+package com.javastart._wrappers;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer firstNumber = new Integer(10);
+        Integer secondNumber = new Integer(10);
+
+        int a = 15;
+        int b = 15;
+
+        System.out.println(firstNumber == secondNumber);
+        System.out.println(a == b);
+    }
+}
